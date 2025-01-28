373adbdad9e9118811837811646c6eccfaf24888
paulayuba <paulayuba788@gmail.com>
2025-01-28 14:21:28 +0100

    correction of icons on the fooert page

--- app/(default)/about/page.tsx
+++ app/(default)/about/page.tsx
@@ -18,31 +18,32 @@ export default function About() {
           forward-thinking technology positions it as a company of the future.
         </p>
       </div>
-      <div className="flex flex-col-reverse md:flex-row md:-space-x-32 items-center px-4 md:px-16  mt-10 space-y-8 md:space-y-0 ">
-        
-        <div className="flex-1 bg-gray-900/25 h-auto md:h-[495px] rounded-md p-8 md:p-20">
-          <h1 className="text-3xl font-semibold mb-8">
-            Innovating the Future Through Collaboration and Technology
-          </h1>
-          <p className="font-semibold leading-relaxed">
-            Bluehouse fosters an environment where ideas flourish, diversity is
-            celebrated, and meaningful contributions are made. Positioned as a
-            company of the future, Bluehouse is shaping industries and creating
-            impactful solutions that leave a lasting legacy.
-          </p>
-        </div>
+      <div className="flex flex-col-reverse md:flex-row md:items-center px-4 md:px-16  space-y-8 md:space-y-0">
+  {/* Text Section */}
+  <div className="flex-1 bg-gray-900/25 mb-8 h-auto md:h-[495px] rounded-md p-8 md:p-20">
+    <h1 className="text-3xl md:text-4xl font-semibold mb-6 md:mb-8">
+      Innovating the Future Through Collaboration and Technology
+    </h1>
+    <p className="font-medium leading-relaxed">
+      Bluehouse fosters an environment where ideas flourish, diversity is
+      celebrated, and meaningful contributions are made. Positioned as a
+      company of the future, Bluehouse is shaping industries and creating
+      impactful solutions that leave a lasting legacy.
+    </p>
+  </div>
 
-        
-        <div className="flex-1 flex justify-center md:justify-end md:pt-32 ">
-          <Image
-            src={aboutImage}
-            alt="Descriptive Alt Text"
-            width={600}
-            height={500}
-            className="rounded-md"
-          />
-        </div>
-      </div>
+  {/* Image Section */}
+  <div className="flex-1 flex justify-center md:justify-end md:pt-32">
+    <Image
+      src={aboutImage}
+      alt="Descriptive Alt Text"
+      width={600}
+      height={500}
+      className="rounded-md shadow-lg"
+    />
+  </div>
+</div>
+
     </div>
   );
 }
