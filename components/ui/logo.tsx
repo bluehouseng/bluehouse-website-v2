d79ac6041c99fb7fed36214d18b3967209cb5e0f
jothamardel <ardelmbiplang@gmail.com>
2025-02-01 00:12:56 +0100

    chore:updating application
    Signed-off-by: jothamardel <ardelmbiplang@gmail.com>

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 export default function Logo() {
   return (
-    <Link href="/" className="inline-flex" aria-label="Cruip">
-     <img src="https://www.bluehouseng.com/assets/bluehouse-cd68d9d4.png" alt="" width='50%' />
+    <Link href='/' className='inline-flex' aria-label='Cruip'>
+      <img
+        src='https://bluehouse-dpf9chkb3-jotham-ardels-projects.vercel.app/assets/bluehouse-cd68d9d4.png'
+        alt=''
+        width='50%'
+      />
     </Link>
   );
 }
