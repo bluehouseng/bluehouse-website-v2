7c4cebd8c873da8c61b743cbfd6d9ad5fbbd25c8
jothamardel <ardelmbiplang@gmail.com>
2025-02-01 00:01:47 +0100

    chore: updating application
    Signed-off-by: jothamardel <ardelmbiplang@gmail.com>

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,17 +1,16 @@
 export const metadata = {
-  title: "Home - Bluehouse",
-  description: "Page description",
+  title: 'Bluehouse',
+  description: 'Learn | Innovate | Impact',
 };
 
-import Hero from "@/components/hero-home";
-import BusinessCategories from "@/components/business-categories";
-import LargeTestimonial from "@/components/large-testimonial";
-import FeaturesPlanet from "@/components/features-planet";
-import Features from "@/components/features-home";
-import TestimonialsCarousel from "@/components/testimonials-carousel";
-import Cta from "@/components/cta";
-import Partners from "@/components/partners";
-
+import Hero from '@/components/hero-home';
+import BusinessCategories from '@/components/business-categories';
+import LargeTestimonial from '@/components/large-testimonial';
+import FeaturesPlanet from '@/components/features-planet';
+import Features from '@/components/features-home';
+import TestimonialsCarousel from '@/components/testimonials-carousel';
+import Cta from '@/components/cta';
+import Partners from '@/components/partners';
 
 export default function Home() {
   return (
