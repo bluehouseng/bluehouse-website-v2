a6f2be5964d02e30b56343722353f2b6be80511e
Petergya <petergyang82@gmail.com>
2025-02-03 15:23:08 +0100

    fixes

--- components/cta.tsx
+++ components/cta.tsx
@@ -3,7 +3,7 @@ import Stripes from "@/public/images/stripes-dark.svg";
 import Link from "next/link";
 export default function Cta() {
   return (
-    <section>
+    <section className="mt-12">
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
         <div
           className="relative overflow-hidden rounded-2xl text-center shadow-xl before:pointer-events-none before:absolute before:inset-0 before:-z-10 before:rounded-2xl before:bg-gray-900"
@@ -30,7 +30,7 @@ export default function Cta() {
             <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
               <Link
 
-              href="https://www.bluelearn.africa/"
+              href="https://forms.gle/14znUV5fWGwrWFG58"
                 target="_blank"
                 className="btn group mb-4 w-full bg-gradient-to-t from-blue-400 to-blue-600 bg-[length:100%_100%] bg-[bottom] text-white shadow hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
                 
