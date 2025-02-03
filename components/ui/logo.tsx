09ac4f502e8d59138533973c9ee67bf704c19b95
Petergya <petergyang82@gmail.com>
2025-02-03 17:09:54 +0100

    fixes

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -5,7 +5,7 @@ export default function Logo() {
       <img
         src='https://bluehouse-dpf9chkb3-jotham-ardels-projects.vercel.app/assets/bluehouse-cd68d9d4.png'
         alt=''
-        width='50%'
+        width='30%'
       />
     </Link>
   );
