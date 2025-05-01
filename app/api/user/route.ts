ea999048fc3d5afff2c6ff156c9ec97a71f2dc94
jothamardel <ardelmbiplang@gmail.com>
2025-05-01 18:30:47 +0100

    updating application
    Signed-off-by: jothamardel <ardelmbiplang@gmail.com>

---
+++ app/api/user/route.ts
@@ -0,0 +1,119 @@
+import axios from "axios";
+import User from "@/model/user";
+import dbConnect from "@/lib/mongoose";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  // Parse the request body
+  const body = await request.json();
+  const { name, email, stack, checkInTime, checkOutTime } = body;
+
+  // check the db to see if the user has record for that day
+  // doesn't - create a record
+  // does checkOutTime
+  //
+
+  console.log({
+    name,
+    email,
+    stack,
+    checkInTime,
+    checkOutTime,
+  });
+  try {
+    const user = await User.findOneAndUpdate(
+      {
+        email,
+        checkInTime,
+      },
+      { checkInTime, checkOutTime },
+    );
+    console.log(user);
+
+    if (!user) {
+      const newUser = new User({ name, email, stack, checkInTime });
+      await newUser.save();
+      return new Response(
+        JSON.stringify({
+          message: "User checked in.",
+          success: true,
+        }),
+        {
+          status: 201,
+          headers: { "Content-Type": "application/json" },
+        },
+      );
+    }
+
+    // user.checkOutTime = checkOutTime;
+    // await user.save();
+
+    return new Response(
+      JSON.stringify({
+        message: "User checked out.",
+        success: true,
+      }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  } catch (err) {
+    console.error(err);
+    return new Response(JSON.stringify({ message: "Unable to update", err }), {
+      status: 400,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
+
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  // Access query parameters
+  const email = url.searchParams.get("email");
+
+  console.log(email); // Lo
+
+  try {
+    if (!email) {
+      return new Response(
+        JSON.stringify({
+          message: "Unable to get user history",
+          success: false,
+        }),
+        {
+          status: 400,
+          headers: { "Content-Type": "application/json" },
+        },
+      );
+    }
+
+    const foundUser = await User.find({
+      email,
+    });
+
+    return new Response(
+      JSON.stringify({
+        message: "User history",
+        success: true,
+        history: foundUser,
+      }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  } catch (err) {
+    console.error(err);
+    return new Response(
+      JSON.stringify({
+        message: "Sorry, something went wrong",
+        success: false,
+      }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  }
+}
---
+++ model/user.js
@@ -0,0 +1,34 @@
+// models/User.js
+import mongoose from "mongoose";
+
+/*
+  Check if the User model is already defined to prevent
+  the "Cannot overwrite model once compiled" error during hot reloading
+*/
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+    maxlength: [60, "Name cannot be more than 60 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+  },
+  stack: {
+    type: String,
+    required: [true, "Please provide a stack"],
+  },
+  checkInTime: {
+    type: Date,
+  },
+  checkOutTime: {
+    type: Date,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
